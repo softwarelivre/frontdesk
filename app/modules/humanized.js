8ea6420ba173dcadc5231fd7c0d82dae7be6514f
Felipe Mobus <fmobus@gmail.com>
2015-07-05 22:45:18 -0300

    showing corp and gov name; formatting doccument properly; fixing ansible role on playbook

--- app/modules/humanized.js
+++ app/modules/humanized.js
@@ -4,17 +4,21 @@
   angular
     .module("segue.frontdesk.humanized", [ 'segue.frontdesk' ])
     .constant("HumanizedStrings", {
-       'business':          'corporativo',
-       'caravan':           'caravanista',
-       'caravan-leader':    'líder de caravana',
-       'foreigner':         'estrangeiro',
-       'foreigner-student': 'estrangeiro estudante',
-       'government':        'empenho',
-       'normal':            'individual',
-       'promocode':         'código promocional',
-       'proponent':         'proponente',
-       'proponent-student': 'proponente estudante',
-       'speaker':           'palestrante',
-       'student':           'estudante'
+        // categories
+        'business':          'corporativo',
+        'caravan':           'caravanista',
+        'caravan-leader':    'líder de caravana',
+        'foreigner':         'estrangeiro',
+        'foreigner-student': 'estrangeiro estudante',
+        'government':        'empenho',
+        'normal':            'individual',
+        'promocode':         'código promocional',
+        'proponent':         'proponente',
+        'proponent-student': 'proponente estudante',
+        'speaker':           'palestrante',
+        'student':           'estudante',
+
+        // payment types
+        'cash':      'dinheiro',
     });
 })();
